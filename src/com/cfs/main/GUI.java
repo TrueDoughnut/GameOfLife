25cd2a4ac8e490787d8c1d372eb974ad65dca35b
TrueDoughnut <Christopher543sap@gmail.com>
2018-09-04 22:13:59 -0500

    Initial commit

---
+++ src/com/cfs/main/GUI.java
@@ -0,0 +1,58 @@
+package com.cfs.main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private Runner runner;
+
+    public GUI(){
+        runner = new Runner();
+    }
+    public GUI(int[][] board){
+        runner = new Runner(board);
+    }
+
+    private JFrame frame = new JFrame("Game of Life");
+    private Timer timer = new Timer(100, this);
+
+    public void start(){
+        createAndShowGUI();
+        timer.start();
+    }
+
+    private void createAndShowGUI(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(runner.getBoard().length, runner.getBoard()[0].length, 5, 5));
+        run();
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void run(){
+        for(int i = 0; i < runner.getBoard().length; i++){
+            for(int j = 0; j < runner.getBoard()[i].length; j++){
+                JPanel panel = new JPanel();
+                if(runner.getBoard()[i][j] == 0){
+                    panel.setBackground(Color.WHITE);
+                } else {
+                    panel.setBackground(Color.BLACK);
+                }
+                frame.add(panel);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        frame.getContentPane().removeAll();
+        frame.revalidate();
+        runner.run();
+        run();
+        frame.pack();
+        frame.repaint();
+    }
+}
---
+++ src/com/cfs/main/Main.java
@@ -0,0 +1,125 @@
+package com.cfs.main;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args){
+        GUI gui = new GUI();
+        gui.start();
+    }
+
+}
+
+class Runner{
+
+    int[][] board;
+    private int cycles;
+
+    Runner(){
+        createRandom();
+    }
+
+    public Runner(int[][] board){
+        //must be rectangular matrix
+        for(int i = 0; i < board.length-1; i++){
+            if(board[i].length != board[i+1].length){
+                System.out.println("Must be rectangular matrix");
+                System.exit(0);
+            }
+        }
+        this.board = board;
+    }
+
+    void run(){
+        ArrayList<ArrayList<Integer>> change = check();
+        change(change);
+    }
+
+    private void createRandom(){
+        board = new int[20][20];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                board[i][j] = new Random().nextInt(2); // 1 (alive) or 0 (dead)
+            }
+        }
+    }
+
+    private ArrayList<ArrayList<Integer>> check(){
+        ArrayList<ArrayList<Integer>> change = new ArrayList<>();
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 1) {
+                    if(getNeighbors(i, j) < 2) {
+                        change.add(coords(i, j));
+                    } else if(getNeighbors(i, j) > 3) {
+                        change.add(coords(i, j));
+                    }
+                } else {
+                    if(getNeighbors(i, j) == 3){
+                        change.add(coords(i, j));
+                    }
+                }
+            }
+        }
+        cycles++;
+        if(change.isEmpty()){
+            System.out.println(cycles);
+            return null;
+        }
+        return change;
+    }
+
+    private void change(ArrayList<ArrayList<Integer>> change){
+        for(ArrayList<Integer> list : change){
+            int x = list.get(0);
+            int y = list.get(1);
+            if(board[x][y] == 0){
+                board[x][y] = 1;
+            } else{
+                board[x][y] = 0;
+            }
+        }
+        
+    }
+
+    private ArrayList<Integer> coords(int x, int y){
+        ArrayList<Integer> coords = new ArrayList<>();
+        coords.add(x);
+        coords.add(y);
+        return coords;
+    }
+
+    private int getNeighbors(int x, int y){
+        int count = 0;
+        try {
+            for(int i = x-1; i <= x+1; i++){
+                for(int j = y-1; j <= y+1; j++){
+                    if(board[i][j] == 1){
+                        count++;
+                    }
+                }
+            }
+        } catch(IndexOutOfBoundsException ignore){}
+        return count;
+    }
+
+    public int[][] getBoard(){
+        return board;
+    }
+
+    @Override
+    public String toString(){
+        String str = "";
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                str += board[i][j] + " ";
+            }
+            str += "\n";
+        }
+
+        return str + "\n";
+    }
+}
